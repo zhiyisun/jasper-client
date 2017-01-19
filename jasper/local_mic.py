db87852a5e14064fd47b17feb9329bf587372731
Zhiyi Sun <zhiyisun@gmail.com>
2017-01-19 17:34:41 +0800

    Add special mode for text.
    Signed-off-by: Zhiyi Sun <zhiyisun@gmail.com>

--- jasper/local_mic.py
+++ jasper/local_mic.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import contextlib
+
 """
 A drop-in replacement for the Mic class that allows for all I/O to occur
 over the terminal. Useful for debugging. Unlike with the typical Mic
@@ -25,3 +27,11 @@ class Mic(object):
 
     def say(self, phrase, OPTIONS=None):
         print("JASPER: %s" % phrase)
+
+    @contextlib.contextmanager
+    def special_mode(self, name, phrases):
+        try:
+            pass
+            yield
+        finally:
+            pass
